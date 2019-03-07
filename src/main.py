fefa7b26fbb5dd08b6b33a50439d145da9aec645
alleydragon <jenniferlove@comcast.net>
2019-03-07 16:26:37 -0500

    Added test call and print

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@
 
 if __name__ == '__main__':
 	print("Hello World")
+
+
+valnums = []
+for i in range(2000,3200):
+	if isValidNumber(i) == True:
+		valnums.append(i)
+
+for i in range(len(valnums)):
+	if i != len(valnums):
+		print(str(valnums[i]) + ", ")
+	else:
+		print(str(valnums[i]))
