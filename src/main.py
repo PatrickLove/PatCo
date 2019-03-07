17fd9e8c6a6fff18abcca9e6766e8381edbff719
Patrick Love <patricklove@comcast.net>
2019-03-07 16:14:47 -0500

    Add documentation, refactor for readability

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-def isDivisibleSeven(n):
-	s = str(n)
-	s = s[1:] if s[0]=='-' else s
-	if(len(s)<=1):
+def isDivisibleSeven(n): # Based on Test  here: 2https://www.math.hmc.edu/funfacts/ffiles/10005.5.shtml
+	s = str(n) # Get digits as char array (string)
+	s = s[1:] if s[0]=='-' else s # Remove negative sign if present
+	if(len(s)<=1): # Base case single digit check
 		return s=='0' or s=='7'
-	else:
+	else: # Split and subtract recursively
 		return isDivisibleSeven(int(s[:-1])-2*int(s[-1]))
 
 def isValidNumber(n):
-	return str(n)[-1]!='5' and str(n)[-1]!='0' and isDivisibleSeven(n)
+	# Check if multiple of 5 by looking at last digit
+	isMultFive = str(n)[-1]=='5' or str(n)[-1]=='0'
+	# Call seven divisibility algorithm
+	isDivSeven = isDivisibleSeven(n)
+	# Compute validity as per requirements
+	return isDivSeven and not isMultFive
 
 if __name__ == '__main__':
 	print("Hello World")
